793d945d5f142c651674c94d39372ae836af0c8c
TheFreeOne <1357441309@qq.com>
2020-08-05 16:41:24 +0800

    添加notification

---
+++ src/org/freeone/ApiDocGeneratorAction.java
@@ -0,0 +1,18 @@
+package org.freeone;
+
+import com.intellij.notification.*;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ApiDocGeneratorAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+
+        NotificationGroup notificationGroup = new NotificationGroup("notificationGroup", NotificationDisplayType.BALLOON, true);
+        Notification notification = notificationGroup.createNotification("notification", NotificationType.ERROR);
+        Notifications.Bus.notify(notification);
+
+    }
+}
